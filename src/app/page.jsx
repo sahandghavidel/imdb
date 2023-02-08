33eceff249090c119366047423cb81fa2f1c5d76
Sahandgha <sahand.gha@gmail.com>
2023-02-09 06:40:29 +0700

    Fix the problem for "search params object is empty in production (Vercel) with next 13 app dir"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,9 @@
+// Fix the problem for "search params object is empty in production with next 13 app dir"
+
+// for more info "https://github.com/vercel/next.js/issues/43077"
+
+export const dynamic = "force-dynamic"; // this is the fix
+
 import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY;
